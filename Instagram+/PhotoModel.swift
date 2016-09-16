7169ead379490e71417ef02c33e2ba3afbdc747c
Amanda McMaster <AmandaMac@Amandas-MacBook-Pro.local>
2016-09-16 13:23:36 -0700

    first commit

---
+++ Instagram+/PhotoModel.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoModel.swift
+//  Instagram+
+//
+//  Created by Amanda McMaster on 9/12/16.
+//  Copyright © 2016 McMaster. All rights reserved.
+//
+
+import Foundation
+
+class PhotoModel {
+    var photos = []
+}
